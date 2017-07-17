630e2276ca83a1ff0b6bd83428d61d1fe64b6dae
JansonHuang <jansonhuang1989@gmail.com>
2017-07-17 15:42:52 +0800

    1.env: add toolchain environment script.

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Notice: please download related toolchain to the toochain directory
+
+TOOLCHAIN_VERSION=gcc-arm-none-eabi-4_9-2015q3
+
+
+export PATH=$PATH:./toolchain/$TOOLCHAIN_VERSION/bin
